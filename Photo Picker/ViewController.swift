57b74e7a60370c0433c8cbcf1a267e49e4ad4f04
Putek-creator <lukii20012@gmail.com>
2022-05-17 22:22:52 +0200

    Basic Photo Picker

--- Photo Picker/ViewController.swift
+++ Photo Picker/ViewController.swift
@@ -4,16 +4,63 @@
 //
 //  Created by Łukasz  Putkowski on 12/05/2022.
 //
-
+import Photos
+import PhotosUI
 import UIKit
 
-class ViewController: UIViewController {
 
+class ViewController: UIViewController, PHPickerViewControllerDelegate {
+    @IBOutlet weak var imageView: UIImageView!
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        if photoCounter == maxPhotoNumber - 1{
+            print("No more photos")
+        }else{
+            photoCounter += 1
+            imageView.image = images[photoCounter]
+        }
+}
+    var photoCounter = 0
+    var maxPhotoNumber = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        title = "New Photo Picker"
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(didTapAdd))
+        
+    }
+    
+    @objc private func didTapAdd(){
+        var config = PHPickerConfiguration(photoLibrary: .shared())
+        config.selectionLimit = 3
+        config.filter = .images
+        let vc = PHPickerViewController(configuration: config)
+        vc.delegate = self
+        present(vc, animated: true)
+        title = "Add more photos"
     }
 
-
+    func picker(_ picker: PHPickerViewController, didFinishPicking results: [PHPickerResult]) {
+        picker.dismiss(animated: true, completion: nil)
+        let group = DispatchGroup()
+        
+        
+        results.forEach { result in
+            group.enter()
+            result.itemProvider.loadObject(ofClass: UIImage.self) {[weak self] reading, error in
+                defer{
+                    group.leave()
+                }
+                guard let image = reading as? UIImage, error == nil else{
+                    return
+                }
+                self?.images.append(image)
+            }
+        }
+        group.notify(queue: .main) {
+            
+            self.maxPhotoNumber = self.images.count
+            self.imageView.image = self.images[self.photoCounter]
+        }
+    }
+    private var images = [UIImage]()
 }
-
